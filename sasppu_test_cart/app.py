dd0a963808023f94558d54fcdce38086508f3eeb
Molive <moliveofscratch@gmail.com>
2025-03-28 00:54:37 +0000

    Text drawing confirmed functional

--- sasppu_test_cart/app.py
+++ sasppu_test_cart/app.py
@@ -1,3 +1,4 @@
+import asyncio
 from app import SASPPUApp
 import sasppu
 import math
@@ -8,6 +9,7 @@ from events.input import BUTTON_TYPES, ButtonDownEvent
 from system.eventbus import eventbus
 from system.scheduler.events import RequestStopAppEvent
 
+BLACK = 0
 WHITE = 0b0111111111111111
 RED = 0b0111110000000000
 GREEN = 0b0000001111100000
@@ -24,6 +26,14 @@ class SASPPUTest(SASPPUApp):
         self.ms.bind()
         self.cs = sasppu.CMathState()
         self.cs.bind()
+        self.init_bg0_map()
+        sasppu.draw_text_background(90, 90, WHITE, 150, "Hello, World!", True)
+
+    def init_bg0_map(self):
+        for x in range(sasppu.MAP_WIDTH):
+            for y in range(sasppu.MAP_HEIGHT):
+                index = ((y % 32) * 256 * 8) + ((x % 32) * 8)
+                sasppu.sasppuinternal_bg0_map(x, y, index)
 
     def _cleanup(self):
         eventbus.remove(ButtonDownEvent, self._handle_buttondown, self)
@@ -33,16 +43,18 @@ class SASPPUTest(SASPPUApp):
     async def run(self, render_update):
         while not self.exit:
             await render_update()
+            #await asyncio.sleep(1)
 
     def draw(self):
         cur_time = time.ticks_ms()
-        self.ms.mainscreen_colour = WHITE
+        self.ms.mainscreen_colour = BLACK
         self.ms.bgcol_windows = sasppu.WINDOW_A
-        self.ms.flags = sasppu.MainState.CMATH_ENABLE
+        self.ms.flags = sasppu.MainState.BG0_ENABLE
+        #self.ms.flags = sasppu.MainState.CMATH_ENABLE
         #ms.window_1_left = int((math.sin(cur_time / 1300.0) + 1) * 64)
         #ms.window_1_right = int((math.cos(cur_time / 1500.0) + 3) * 64)
-        self.cs.flags = sasppu.CMathState.FADE_ENABLE
-        self.cs.fade = int((math.sin(cur_time / 1000.0) + 1) * 127)
+        #self.cs.flags = sasppu.CMathState.FADE_ENABLE
+        #self.cs.fade = int((math.sin(cur_time / 1000.0) + 1) * 127)
         print("fps:", display.get_fps())
 
     def _handle_buttondown(self, event: ButtonDownEvent):
